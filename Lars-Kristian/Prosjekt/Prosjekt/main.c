a58fb27f5310dfe6655a8ef77d69db2c5ab12a93
Lars-Kristian <lars_matti@hotmail.com>
2024-04-25 17:25:04 +0200

    update

--- Lars-Kristian/Prosjekt/Prosjekt/main.c
+++ Lars-Kristian/Prosjekt/Prosjekt/main.c
@@ -7,22 +7,21 @@
 #include <stdio.h>	// For printf
 #include <stdbool.h> // For ADC
 #include <avr/interrupt.h>
-//#include <avr/eeprom.h>
+#include <avr/eeprom.h>
 
 #include "TCn.h" // Include own library with TCA and TCB functions
 #include "ADCn.h" // Include own library with ADC
 #include "USARTn1.h"
 
 // Preprocessing
-void PORT_init(void);
-uint16_t PWM_freq_to_RPM(uint16_t PWM_onperiode);
+uint32_t PWM_freq_to_RPM(uint32_t PWM_onperiode);
 void RTC_init(void);
+void average_values_for_tach(void);
 
 // Variables
-volatile uint16_t RPM_fan1;
-volatile uint16_t RPM_fan2;
+volatile uint32_t RPM_fan1;
+volatile uint32_t RPM_fan2;
 volatile uint16_t counter;
-volatile uint16_t delay_counter = 0;
 
 
 void RTC_init(void)
@@ -40,9 +39,7 @@ void RTC_init(void)
 // RTC interrupt
 ISR(RTC_PIT_vect)
 {
-	
-	counter ++;
-	
+	counter ++; // Counter represent seconds
 	RTC.PITINTFLAGS = RTC_PI_bm; // Clears the interrupt flag
 }
 // TCB0 interrupt function
@@ -54,7 +51,6 @@ ISR(TCB0_INT_vect)
 	//TCB0.CCMP // holds the compare, capture, and top value
 	RPM_fan1= PWM_freq_to_RPM(TCB0.CCMP);
 	TCB0.INTFLAGS = TCB_CAPT_bm; // Clear the interrupt flag
-	
 }
 
 ISR(TCB1_INT_vect)
@@ -87,7 +83,7 @@ int main(void)
 	{
 		if(counter == 20)
 		{
-			values_for_tach();
+			average_values_for_tach();
 			counter = 0;
 		}
 		if (ADC0_conersionDone())
@@ -110,15 +106,15 @@ int main(void)
 	}
 }
 
-uint16_t PWM_freq_to_RPM(uint16_t PWM_onperiode)
+uint32_t PWM_freq_to_RPM(uint32_t PWM_onperiode)
 {
 	return ((F_CPU * 60)/(4 * 4 * PWM_onperiode));
 }
 
-// Rewrite for multiple fans, with fannr, max fan speed as parameter and return average
-void values_for_tach()
+// For predicting failures with fans
+void average_values_for_tach()
 {
-	// SETT DETTE I IF MED FLAGG
+	// SETT DETTE I IF MED FLAGG???
 	
 	// Set value to all fans
 	TCA0.SPLIT.LCMP0 = DUTY_CYCLE_MAX_VALUE;
@@ -128,33 +124,37 @@ void values_for_tach()
 	
 	printf("Satt vifter! \r\n");
 	
-	uint32_t sum_RPM = 0;
-	uint16_t average_read_value;
-	uint32_t num_of_readings = 0;
+	// For average read values for two fans
+	uint32_t sum_RPM_fan1 = 0;
+	uint32_t sum_RPM_fan2 = 0;
+	uint32_t average_read_value_fan1;
+	uint32_t average_read_value_fan2;
 	
-	volatile uint16_t compare_counter_time = counter;
+	uint32_t num_of_readings = 0;
+	uint16_t compare_counter_time = counter; // reset counter for compare
 	
 	while (1)
 	{
-		//sum_RPM += PWM_freq_to_RPM(TCB0.CCMP);
-		sum_RPM += RPM_fan1;
+		sum_RPM_fan1 += RPM_fan1;
+		sum_RPM_fan2 += RPM_fan2;
+		
 		printf("PWM read: %d\r\n", RPM_fan1);
 		num_of_readings ++;
 		
+		// Run test in x seconds
 		if (counter - compare_counter_time >= 10)
 		{
 			break;
 		}
 	}
 		
-	average_read_value = sum_RPM / num_of_readings;
+	average_read_value_fan1 = sum_RPM_fan1 / num_of_readings;
+	average_read_value_fan2 = sum_RPM_fan2 / num_of_readings;
 	
 	printf("Number of readings: %d\r\n", num_of_readings);
-	printf("Gjennomsnitt: %d\r\n", average_read_value);
+	printf("Gjennomsnitt: %d\r\n", average_read_value_fan1);
 	
 	printf("\r\n");
 	printf("\r\n");
-
-	// Maybe return a value
-	
 }
+
