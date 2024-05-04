52282a7859cefb57c1b3c444e0edaddd3fe48adf
Jarlenum <jarle.nummedal@hotmail.com>
2024-05-04 13:00:43 +0200

    idk

---
+++ Jarle/Jarle 28.04/Mikro_Prosjekt_Ferdig/TCn.h
@@ -0,0 +1,21 @@
+/*
+ * TCn.h
+ *
+ * Created: 4/28/2024 10:32:14 AM
+ *  Author: Kaspe
+ */ 
+
+
+#ifndef TCN_H_
+#define TCN_H_
+
+void TCA0_init(void);
+void TCB0_init(void);
+void PORT_init(void);
+void TCB1_init(void);
+
+
+
+
+
+#endif /* TCN_H_ */
